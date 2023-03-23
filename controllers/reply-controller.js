aa1ed960b7e12b240213f64535bd335730b3b3fc
peggy8865 <peggy8865@gmail.com>
2023-03-23 18:13:44 +0800

    fix: add error messages to postReply

--- controllers/reply-controller.js
+++ controllers/reply-controller.js
@@ -7,8 +7,10 @@ const replyController = {
     try {
       const TweetId = req.params.tweet_id
       const UserId = helper.getUser(req).id
+      if (!UserId) throw new Error('使用者不存在')
       const { comment } = valueTrim(req.body)
       if (!comment) throw new Error('回覆不可空白')
+      if (comment.length > 140) throw new Error('字數限制 140 字')
       const tweet = await Tweet.findByPk(TweetId, { raw: true })
       if (!tweet) throw new Error('推文不存在')
       await Reply.create({ TweetId, UserId, comment })
